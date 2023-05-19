a7bf72af9bb793010f218d4a869b85afaa3e3e39
Sylkos <sylkos49@gmail.com>
2023-05-19 19:18:38 -0400

    Implemented SrFileGenerator. Fixes #3

---
+++ src/main/java/cs3500/pa01/SrFileGenerator.java
@@ -0,0 +1,59 @@
+package cs3500.pa01;
+
+import cs3500.pa01.files.FileGenerator;
+import cs3500.pa01.files.QuestionFile;
+import cs3500.pa01.spacedrep.Question;
+import java.util.ArrayList;
+
+/**
+ * Generates the String form of an SR file given a file that contains questions
+ */
+public class SrFileGenerator implements FileGenerator {
+
+  /**
+   * The files to extract data from
+   */
+  private final ArrayList<QuestionFile> files;
+
+  /**
+   * Constructor
+   *
+   * @param files The files used to source the questions
+   */
+  SrFileGenerator(ArrayList<QuestionFile> files) {
+    this.files = files;
+  }
+
+  SrFileGenerator(QuestionFile file) {
+    this.files = new ArrayList<>();
+    this.files.add(file);
+  }
+
+  /**
+   * Generate the contents of a file
+   *
+   * @return A String representing the contents of a file to be written
+   */
+  @Override
+  public String generate() {
+    StringBuilder sb = new StringBuilder();
+    for (Question q : this.allQuestions()) {
+      sb.append(q);
+      sb.append("\n");
+    }
+    return sb.toString();
+  }
+
+  /**
+   * Gets all the questions contained in a list of files
+   *
+   * @return An ArrayList of all questions in the files
+   */
+  private ArrayList<Question> allQuestions() {
+    ArrayList<Question> questions = new ArrayList<>();
+    for (QuestionFile f : this.files) {
+      questions.addAll(f.getQuestions());
+    }
+    return questions;
+  }
+}
