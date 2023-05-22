942e87a4942f7a8d837512d02b2cffb893f09722
Sylkos <sylkos49@gmail.com>
2023-05-22 12:15:28 -0400

    Added a test for flipping question type.

--- src/test/java/cs3500/pa01/spacedrep/SessionTest.java
+++ src/test/java/cs3500/pa01/spacedrep/SessionTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 class SessionTest {
   Question q1;
+  Question q2;
   QuestionBank qb;
   Session session;
 
@@ -17,11 +18,11 @@ class SessionTest {
   @BeforeEach
   public void setup() {
     this.q1 = new Question(QuestionType.EASY, "Q1Q", "Q1A");
-    Question q2 = new Question(QuestionType.HARD, "Q2Q", "Q2A");
+    this.q2 = new Question(QuestionType.HARD, "Q2Q", "Q2A");
     Question q3 = new Question(QuestionType.EASY, "Q3Q", "Q3A");
     ArrayList<Question> questions = new ArrayList<>();
     questions.add(this.q1);
-    questions.add(q2);
+    questions.add(this.q2);
     questions.add(q3);
     SrFile sr = new SrFile(questions);
     ArrayList<QuestionFile> files = new ArrayList<>();
@@ -50,5 +51,25 @@ class SessionTest {
         1 questions flipped from easy to hard.
         Currently there are 2 hard questions.
         Currently there are 1 easy questions.""");
+  }
+
+  @Test
+  public void testToEasy() {
+    // Does nothing, since this question doesn't change state
+    this.session.questionToHard(this.q2);
+    assertEquals(this.session.summary(), """
+        You answered 0 questions.
+        0 questions flipped from hard to easy.
+        0 questions flipped from easy to hard.
+        Currently there are 1 hard questions.
+        Currently there are 2 easy questions.""");
+
+    this.session.questionToEasy(this.q2);
+    assertEquals(this.session.summary(), """
+        You answered 0 questions.
+        1 questions flipped from hard to easy.
+        0 questions flipped from easy to hard.
+        Currently there are 0 hard questions.
+        Currently there are 3 easy questions.""");
   }
 }
